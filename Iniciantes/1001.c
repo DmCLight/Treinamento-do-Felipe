1aca11a36f78aa0174a6a2030f114c11e761282e
DmCLight <denis.silva_pereira@outlook.com>
2017-02-28 00:50:06 -0300

    Iniciantes

---
+++ Iniciantes/1001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+  int numero_a, numero_b, resultado;
+
+  scanf("%d", &numero_a);
+  scanf("%d", &numero_b);
+
+  resultado = numero_a + numero_b;
+
+  printf("X = %d\n", resultado);
+}
