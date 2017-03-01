7d78d1e7f77e526f9167c594c753968b13ba774f
DmCLight <denis.silva_pereira@outlook.com>
2017-02-28 22:22:39 -0300

    Novos exercicios resolvidos

--- Iniciantes/atualizar_remoto.sh
+++ Iniciantes/atualizar_remoto.sh
@@ -1,3 +1,6 @@
+@echo "Adicionando arquivos alterados"
 git add .
+@echo "Adicionando mensagem de interação."
 git commit -m "Novos exercicios resolvidos"
+@echo "Enviando para o GitHub"
 git push origin master
